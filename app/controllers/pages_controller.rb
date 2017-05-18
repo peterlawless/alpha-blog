c1c1cd423451b60e00422b7e45bf49f3f40427bc
Peter Lawless <peterlawless223@gmail.com>
2017-05-18 07:35:56 -0400

    Complete authentication system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
+
+  # redirects logged in users to articles index page so that they do not
+  # get prompted to sign up or log in again
   def home
+    redirect_to articles_path if logged_in?
   end
 
   def about
